4b01242df972dec105230e801ac17bd4f51781a5
Miguel <miguel0102ces@gmail.com>
2025-05-12 06:04:57 -0500

    initial commit

---
+++ app/src/main/java/com/educador/androidmaster/Variables.kt
@@ -0,0 +1,46 @@
+package com.educador.androidmaster
+
+//Variables
+/**
+*Comentarios
+ */
+
+fun main(){
+    //Variables númericas
+
+    val name = "Educador"
+    //Int -2,147,483,647 a 2,147,483,647
+    val age:Int = 38
+
+    //Long -9,223,372,036,854,775,807 a 9,223,372,036,854,775.807
+    val example:Long = 30
+
+    //Float
+    val floatExample:Float = 3.2f
+
+    //Double
+    val doubleExample:Double = 3547.14758
+
+    //Alfanúmericas
+
+    //Char
+    val charExample:Char = 'a'
+    val charExample1:Char = '@'
+    val charExample2:Char = '1'
+
+    //String
+    val stringExample:String = "Hola a todos"
+
+    //Boolean
+    val booleanExample:Boolean = true
+    val booleanExample1:Boolean = false
+
+    //println(stringExample)
+    val edadInt:Int = 35
+    val edadInt2:Int = 35
+    //println(edadInt2)
+    val edadInt3:Int = 19
+    //println(edadInt3)
+    println("Sumar:")
+    println(edadInt + edadInt2 + edadInt3)
+}
---
+++ app/src/main/java/com/educador/androidmaster/firtapp/FirtAppActivity.kt
@@ -0,0 +1,17 @@
+package com.educador.androidmaster.firtapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.educador.androidmaster.R
+
+class FirtAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_firt_app)
+
+    }
+}
